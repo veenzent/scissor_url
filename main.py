4799ffaff71913e2db3be46634ec1155ecf36a9c
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2024-03-05 13:57:46 +0100

    added CORS

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scissorapp.routers.link_shortener import url_shortener
 from scissorapp import models
 from scissorapp.database import Base, engine
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5500"
+    "http://localhost:5501"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(url_shortener)
 
 
