e5417146e08235c5b463c22cc0f7f5d499f0f0bf
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2024-03-06 20:22:01 +0100

    configuring CORS

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from scissorapp.database import Base, engine
 app = FastAPI()
 
 origins = [
-    "http://localhost:5500"
-    "http://localhost:5501"
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
     CORSMiddleware,
+    # allow_origins=["*"],
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 app.include_router(url_shortener)
 
